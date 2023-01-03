70b7c988c85c6b1a2a869427b2e3a32c9c4f942b
Felix Möller <felix.moeller4@gmail.com>
2023-01-03 16:26:13 +0100

    Add first problems of linked list subsection

---
+++ python/reorder_list.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        stack = [head]
+        cur = head
+        length = 1
+        while cur.next:
+            stack.append(cur.next)
+            cur = cur.next
+            length += 1
+
+        if length <= 2:
+            return
+        cur = head
+        for i in range(length):
+            if i%2 == 0:
+                temp = cur.next
+                cur.next = stack[-1]
+                cur = temp
+            else:
+                stack[-1].next = cur
+                stack.pop()
+
+        if length % 2 == 0:
+            cur.next = None
+        else:
+            stack[-1].next = None
---
+++ python/reverse_linked_list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head  or not head.next:
+            return head
+        prev = head
+        cur = prev.next
+        prev.next = None
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        return prev
